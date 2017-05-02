4a621322c86a12fe529e145130277a84407bd37d
Prateek Bhatnagar <prateek89born@gmail.com>
2017-05-02 12:21:32 +0530

    adding toolbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,55 @@
 import { h, Component } from 'preact';
-import { Link } from 'preact-router';
+import { route } from 'preact-router';
+import Toolbar from 'preact-material-components/Toolbar';
+import Drawer from 'preact-material-components/Drawer';
 import style from './style.less';
 
 export default class Header extends Component {
+	closeDrawer() {
+		this.drawer.MDComponent.open = false;
+	}
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>Preact App</h1>
-				<nav>
-					<Link href="/">Home</Link>
-					<Link href="/profile">Me</Link>
-					<Link href="/profile/john">John</Link>
-				</nav>
+				<Toolbar className="toolbar">
+					<Toolbar.Row>
+						<Toolbar.Section align-start={true}>
+							<Toolbar.Icon onClick={()=>{
+									this.drawer.MDComponent.open = true;
+								}}>menu</Toolbar.Icon>
+							<Toolbar.Title>
+								Preact App
+							</Toolbar.Title>
+						</Toolbar.Section>
+					</Toolbar.Row>
+				</Toolbar>
+				<Drawer.TemporaryDrawer ref={drawer=>{this.drawer = drawer;}} >
+					<Drawer.TemporaryDrawerHeader className="mdc-theme--primary-bg">
+						Navigation
+					</Drawer.TemporaryDrawerHeader>
+					<Drawer.TemporaryDrawerContent>
+						<Drawer.DrawerItem onClick={()=>{
+							this.closeDrawer();
+							route("/");
+						}}>
+							<span href="/">
+								Home
+							</span>
+						</Drawer.DrawerItem>
+						<Drawer.DrawerItem onClick={()=>{
+							this.closeDrawer();
+							route("/profile");
+						}}>
+							<span>Me</span>
+						</Drawer.DrawerItem>
+						<Drawer.DrawerItem onClick={()=>{
+							this.closeDrawer();
+							route("/profile/john");
+						}}>
+							<span>John</span>
+						</Drawer.DrawerItem>
+					</Drawer.TemporaryDrawerContent>
+				</Drawer.TemporaryDrawer>
 			</header>
 		);
 	}
